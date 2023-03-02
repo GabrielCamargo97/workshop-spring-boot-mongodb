ee25ea424b716c3dff3993a4d657d4260a2ab1bb
Gabriel de Camargo <gha_1997@hotmail.com>
2023-03-02 20:11:44 -0300

    Obtendo um post por id

---
+++ src/main/java/com/gacamargo/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.gacamargo.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gacamargo.workshopmongo.domain.Post;
+import com.gacamargo.workshopmongo.repository.postRepository;
+import com.gacamargo.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private postRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
